4aa193be7bc47deba87883d93239c0bc01875259
Barys Smaliak <smboris@yandex.ru>
2023-03-28 08:43:51 +0300

    Example003 added

---
+++ Example003/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+int number = 0, remainder = 0;
+
+Console.WriteLine("Enter number: ");
+
+if (int.TryParse(Console.ReadLine(), out number))
+{
+    remainder = number % 2;
+    if(remainder == 0)
+    {
+        Console.WriteLine("number is even");
+    }
+    else
+    {
+        Console.WriteLine("number is odd");
+    }
+}
+else
+{
+    Console.WriteLine("it,s not a number");
+}
