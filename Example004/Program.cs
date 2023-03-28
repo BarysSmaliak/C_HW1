78854f55fd5fb41c78686825ead047faba00b044
Barys Smaliak <smboris@yandex.ru>
2023-03-28 08:47:03 +0300

    Example004 added

---
+++ Example004/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int N = 0;
+
+Console.WriteLine("Enter number: ");
+
+if(int.TryParse(Console.ReadLine(), out N))
+{
+    for(int i = 1; i <= N; i++)
+    {
+        Console.Write(i);
+        Console.Write(" ");
+    }
+}
+else
+{
+    Console.WriteLine("it,s not a number");
+}
